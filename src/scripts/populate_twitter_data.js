98686a68d36acd754f247f0f82ddc15ce97af617
thefarang <benjamin.vickers@yahoo.co.uk>
2017-10-02 19:01:14 +0700

    Added working tests for data retrieval

--- src/scripts/populate_twitter_data.js
+++ src/scripts/populate_twitter_data.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 let TwitterData = require('../models/twitter-data')
 
 const ObjectId = mongoose.Types.ObjectId
+
+mongoose.Promise = global.Promise
 mongoose.connect('mongodb://mongo:27017/local')  // @todo test database?
 
 new Promise((resolve, reject) => {
@@ -23,14 +25,14 @@ new Promise((resolve, reject) => {
     twitterData.oauth_secret = 'PbKfYqSryyeKDWz4ebtY3o5ogNLG11WJuZBc9fQrQo'
     twitterData.screenname = '@concha_app'
     twitterData.url = 'https://twitter.com/concha_app'
-    twitterData.age = ''
+    twitterData.age = new Date('1970-01-01T00:00:00Z')
     twitterData.save((err) => {
       if (err) {
         return reject(err)
       }
 
-      console.log('Populated Twitter Data')
-      console.log(user._id)
+      console.log('Populated Twitter Data') // 59d1f5e08c75f30072dfcb5e
+      console.log(twitterData._id)
       resolve()
     })
   })
--- test/data-endpoint.js
+++ test/data-endpoint.js
@@ -1,36 +1,77 @@
 'use strict'
 
-// First load in modules
-// Wrap mongoose in Mockgoose
-// Populate with test data
-// Perform tests...
-
 const chai = require('chai')
 const expect = require('chai').expect
 const chaiHttp = require('chai-http')
+const mongoose = require('mongoose')
+const TwitterData = require('../src/models/twitter-data')
 const app = require('../src/app')
 
+const ObjectId = mongoose.Types.ObjectId
 chai.use(chaiHttp)
 
-const conchaUserId = '507f1f77bcf86cd799439011';
+const conchaUserUnknownId = '507f1f77bcf86cd799439010'
+const conchaUserKnownId = '507f1f77bcf86cd799439011'
 
 describe('Data API Endpoint', () => {
-  beforeEach((done) => {
-    done()
+  
+  before(async () => {
+    return new Promise((resolve, reject) => {
+
+      // Connect to the test database
+      mongoose.Promise = global.Promise
+      mongoose.connect('mongodb://mongo:27017/local', { useMongoClient: true }, (err) => {
+        if (err) {
+          return reject(err)
+        }
+        return resolve()
+      })
+
+    })
+    .then(() => {
+
+      return new Promise((resolve, reject) => {
+        // Clear down the test database
+        for (var i in mongoose.connection.collections) {
+          mongoose.connection.collections[i].remove(function() {});
+        }
+        return resolve();
+      })
+    
+    })
+    .then(() => {
+
+      // Populate the test database with test data.
+      return new Promise((resolve, reject) => {
+        const twitterData = new TwitterData()
+        twitterData.concha_user_id = new ObjectId('507f1f77bcf86cd799439011')
+        twitterData.twitter_id = '12345678901234567890'
+        twitterData.oauth_token = '7588892-kagSNqWge8gB1WwE3plnFsJHAZVfxWD7Vb57p0b4&'
+        twitterData.oauth_secret = 'PbKfYqSryyeKDWz4ebtY3o5ogNLG11WJuZBc9fQrQo'
+        twitterData.screenname = 'concha_app'
+        twitterData.url = 'https://twitter.com/concha_app'
+        twitterData.age = new Date('1970-01-01T00:00:00.000Z')
+        twitterData.save((err) => {
+          if (err) {
+            return reject(err)
+          }
+          return resolve()
+        })
+
+      })
+      .catch((err) => {
+        console.log(err)
+        process.exit(0)
+      })
+    })
   })
 
   it ('Should return 404 if user Twitter data does not exist', (done) => {
     chai
       .request(app)
-      // http://chaijs.com/plugins/chai-http/
-      // .post('/user/me')
-      // .set('X-API-Key', 'foobar')
-      // .send({ password: '123', confirmPassword: '123' })
-      // .field('password', '123')
-      .get(`/api/v1/data/${conchaUserId}`)
+      .get(`/api/v1/data/${conchaUserUnknownId}`)
       .set('Accept', 'application/json')
       .end((err, res) => {
-        expect(err).to.not.be.null
         expect(res).to.have.status(404)
         expect(res).to.be.json
         expect(res.text).to.be.empty
@@ -38,25 +79,24 @@ describe('Data API Endpoint', () => {
       })
   })
 
-  /*
   it ('Should return 200 and Twitter data if user Twitter data exists', (done) => {
     chai
       .request(app)
-      // http://chaijs.com/plugins/chai-http/
-      // .post('/user/me')
-      // .set('X-API-Key', 'foobar')
-      // .send({ password: '123', confirmPassword: '123' })
-      // .field('password', '123')
-      .get(`/api/v1/data/${conchaUserId}`)
+      .get(`/api/v1/data/${conchaUserKnownId}`)
       .set('Accept', 'application/json')
       .end((err, res) => {
-        expect(err).to.be.null
+        const responseContents = JSON.parse(res.text)
+
         expect(res).to.have.status(200)
         expect(res).to.be.json
-        console.log('EXTRACT THE CONTENTS OF THE JSON')
-        // expect(res.text).to.include()
+        expect(responseContents.concha_user_id).to.equal('507f1f77bcf86cd799439011')
+        expect(responseContents.twitter_id).to.equal('12345678901234567890')
+        expect(responseContents.oauth_token).to.equal('7588892-kagSNqWge8gB1WwE3plnFsJHAZVfxWD7Vb57p0b4&')
+        expect(responseContents.oauth_secret).to.equal('PbKfYqSryyeKDWz4ebtY3o5ogNLG11WJuZBc9fQrQo')
+        expect(responseContents.screenname).to.equal('concha_app')
+        expect(responseContents.url).to.equal('https://twitter.com/concha_app')
+        expect(responseContents.age).to.equal('1970-01-01T00:00:00.000Z')
         done()
       })
   })
-  */
 })
