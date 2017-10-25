ca7eb4fb65985fda6c2b8e906590b85f1afc1723
thefarang <benjamin.vickers@yahoo.co.uk>
2017-10-25 09:54:50 +0200

    Moved developer test script to the test suite

--- src/test/scripts/reload.js
+++ src/test/scripts/reload.js
@@ -1,5 +1,10 @@
 'use strict'
 
+// * Test script to populate the currently selected database with a test Twitter account
+// * The idea is that developers can then manually interact with the application, testing
+//   retrieval, update etc.
+// * This script is not a formal part of the test suite.
+
 const log = require('../services/log')
 const dbService = require('../services/database/service')
 
@@ -30,7 +35,15 @@ const repopulate = () => {
     return new Promise(async (resolve, reject) => {
       try {
         log.info({}, 'Populating the collection...')
-        await dbService.save(dbService.getDefinitions())
+        await dbService.save({
+          concha_user_id: '507f1f77bcf86cd799439011',
+          twitter_id: '12345678901234567890',
+          oauth_token: '7588892-kagSNqWge8gB1WwE3plnFsJHAZVfxWD7Vb57p0b4&',
+          oauth_secret: 'PbKfYqSryyeKDWz4ebtY3o5ogNLG11WJuZBc9fQrQo',
+          screenname: 'concha_app',
+          url: 'https://twitter.com/concha_app',
+          age: '1970-01-01T00:00:00.000Z'
+        })
         return resolve()
       } catch (err) {
         log.info({ err: err }, 'An error occurred saving the Twitter object')
